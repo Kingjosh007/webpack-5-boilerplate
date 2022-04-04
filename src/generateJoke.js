2a16ba8d8e7081c18167a25811ccc356dc2ef30c
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-04-04 03:07:04 +0100

    Install and setup Webhint, StyleLint and ESLint

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-
 const generateJoke = () => {
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
-    axios.get('https://icanhazdadjoke.com/', config)
-        .then(res => {
-            document.getElementById('joke').innerHTML = res.data.joke;
-        })
-        .catch(err => {
-        console.log(err);
-        return 'Error';
-        });
-}
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  axios.get('https://icanhazdadjoke.com/', config)
+    .then((res) => {
+      document.getElementById('joke').innerHTML = res.data.joke;
+    })
+    .catch((err) => {
+      console.log(err);
+      return 'Error';
+    });
+};
 
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import generateJoke from "./generateJoke";
+import generateJoke from './generateJoke';
 import './styles/main.scss';
 import laughing from './assets/laughing.png';
 
